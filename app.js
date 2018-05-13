20576559e4676851d4374788377fc44feb875411
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-13 00:29:28 -0300

    Corrige Sass, HandleBars, express

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
+const exphbs = require('express-handlebars');
 const app = express()
 
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public/css'),
+    force: true,
+    outputStyle: 'compressed',
+    prefix: '/css'
+}));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.set('port', (process.env.PORT || 3000))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/view/index.html'));
+    res.render('index')
 });
 
 app.listen(app.get('port'),()=>
