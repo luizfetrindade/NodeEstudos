31be2dc61c831b3e15829b7f1023d64f639dffd4
luizfetrindade <luiz.fetrindade@gmail.com>
2018-05-14 19:01:37 -0300

    Adiciona About

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get('/social', (req, res) => {
     res.render('social')
 });
 
+app.get('/tst', (req, res) => {
+    res.render('tst')
+});
+
 app.listen(app.get('port'),()=>
 console.log("Servidor rodando na porta"+app.get('port'))
 )
